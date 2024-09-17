923ec0a221b41d7cd35b0860af4a3c0789b6857d
ZeChen <z.c.wu32@gmail.com>
2024-09-17 10:51:48 +0800

    initialize

---
+++ src/main/java/com/personal_project/Next_to_read/NextToReadApplication.java
@@ -0,0 +1,13 @@
+package com.personal_project.Next_to_read;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NextToReadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NextToReadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/personal_project/Next_to_read/NextToReadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal_project.Next_to_read;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NextToReadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
